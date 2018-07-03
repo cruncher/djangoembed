4bf34c41897cad1f856fcf786a35c6a7ca7c3dfa
Marco Bonetti <marco@cruncher.ch>
2018-07-03 14:21:13 +0200

    fix embed

--- oembed/resources.py
+++ oembed/resources.py
@@ -2,6 +2,7 @@ from django.utils import simplejson
 
 from oembed.exceptions import OEmbedException
 
+
 class OEmbedResource(object):
     """
     OEmbed resource, as well as a factory for creating resource instances
@@ -9,33 +10,36 @@ class OEmbedResource(object):
     """
     _data = {}
     content_object = None
-    
+
     def __getattr__(self, name):
         return self._data.get(name)
-    
+
     def get_data(self):
         return self._data
-        
+
     def load_data(self, data):
         self._data = data
-    
+
     @property
     def json(self):
         return simplejson.dumps(self._data)
-    
+
     @classmethod
     def create(cls, data):
-        if not 'type' in data or not 'version' in data:
+        if 'type' not in data or 'version' not in data:
             raise OEmbedException('Missing required fields on OEmbed response.')
 
-        data['width'] = data.get('width') and int(data['width']) or None
-        data['height'] = data.get('height') and int(data['height']) or None
-        
+        try:
+            data['width'] = data.get('width') and int(data['width']) or None
+            data['height'] = data.get('height') and int(data['height']) or None
+        except:
+            pass
+
         filtered_data = dict([(k, v) for k, v in data.items() if v])
-        
+
         resource = cls()
         resource.load_data(filtered_data)
-        
+
         return resource
 
     @classmethod
